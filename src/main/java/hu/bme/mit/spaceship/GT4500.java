c0af4ba29d4b400f40082e37ee85f357f6d6d154
benana6 <szeman.benjamin@gmail.com>
2022-04-08 08:58:46 +0200

    FireAllFixed

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,18 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+        if (! primaryTorpedoStore.isEmpty()) {
+          firingSuccess = primaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = true;
+        }
+      
+        if (! secondaryTorpedoStore.isEmpty()) {
+            firingSuccess = secondaryTorpedoStore.fire(1);
+            
+          }
+
+          // if both of the stores are empty, nothing can be done, return failure
+        
 
         break;
     }
